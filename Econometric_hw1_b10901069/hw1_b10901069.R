4296f419bed48bc121ba1d5c9f3d3abe6d341de4
Chinying Lin <104905519+cylin00@users.noreply.github.com>
2024-09-05 19:50:49 +0800

    feat: complete problem 2

--- Econometric_hw1_b10901069/hw1_b10901069.R
+++ Econometric_hw1_b10901069/hw1_b10901069.R
@@ -1,19 +1,15 @@
 Data <- read.csv("Equity_Premium.csv")
 Matrix <- as.matrix(Data)
-
 TimeMatrix <- Matrix[, 1, drop = FALSE]
 YMatrix <- Matrix[, 2, drop = FALSE]
 XMatrix <- Matrix[, -c(1, 2), drop = FALSE]
 
-print(XMatrix[, 1]) 
-
-#----------Problem-1----------
-
-#X's Time Series Plot
+#---------- Problem 1 ----------
 TimeSerie <- function(X, yLab){
   return(plot(TimeMatrix, X , type = "l", col = rgb(0, 0, 1), xlab = "Time", ylab = yLab, main = "Time Serie of X's column"))
 }
 
+#X's Time Series Plot
 for(i in 1:ncol(XMatrix)) 
 {
   png(paste(colnames(XMatrix)[i], "_TimeSeriesPlot.png", sep = ""))
@@ -22,15 +18,18 @@ for(i in 1:ncol(XMatrix))
 }
 
 #X's Histogram
-estimatedData <- rnorm(5544)
-Histogram <- function(X){
-  return(hist(X, probability = TRUE, main = "Histogram of X", xlab = "Values", col = rgb(0.1, 0.3, 0.4, 0.5), border = "white", breaks = 20))
-}
-
 png(paste("X_Histogram.png"))
-Histogram(XMatrix)
-density_estimate <- density(estimatedData)
-lines(density_estimate, col = rgb(0.7, 0.1, 0.1, 0.7), lwd = 1.5)
+hist(XMatrix, probability = TRUE, main = "Histogram of X", xlab = "Values", col = rgb(0.1, 0.3, 0.4, 0.5), border = "white", breaks = 20)
+lines(density(rnorm(5544)), col = rgb(0.7, 0.1, 0.1, 0.7), lwd = 1.5)
 dev.off()
 
+#Y's Time Series Plot
+png(paste(colnames(YMatrix), "_TimeSeriesPlot.png", sep = ""))
+TimeSeriesPlot(YMatrix, colnames(YMatrix))
+dev.off()
 
+#Y's Histogram
+png(paste("Y_Histogram.png"))
+hist(YMatrix, probability = TRUE, main = "Histogram of Y", xlab = "Values", col = rgb(0.1, 0.3, 0.4, 0.5), border = "white", breaks = 20)
+lines(density(rnorm(504)), col = rgb(0.7, 0.1, 0.1, 0.7), lwd = 1.5)
+dev.off()
