80cdea1b23308b297f3aea064156f4fdeca552c4
Chinying Lin <104905519+cylin00@users.noreply.github.com>
2024-09-05 20:12:17 +0800

    feat: complete problem 3

--- Econometric_hw1_b10901069/hw1_b10901069.R
+++ Econometric_hw1_b10901069/hw1_b10901069.R
@@ -4,7 +4,8 @@ TimeMatrix <- Matrix[, 1, drop = FALSE]
 YMatrix <- Matrix[, 2, drop = FALSE]
 XMatrix <- Matrix[, -c(1, 2), drop = FALSE]
 
-#---------- Problem 1 ----------
+#-------------------- Problem 1 --------------------
+
 TimeSerie <- function(X, yLab){
   return(plot(TimeMatrix, X , type = "l", col = rgb(0, 0, 1), xlab = "Time", ylab = yLab, main = "Time Serie of X's column"))
 }
@@ -33,3 +34,30 @@ png(paste("Y_Histogram.png"))
 hist(YMatrix, probability = TRUE, main = "Histogram of Y", xlab = "Values", col = rgb(0.1, 0.3, 0.4, 0.5), border = "white", breaks = 20)
 lines(density(rnorm(504)), col = rgb(0.7, 0.1, 0.1, 0.7), lwd = 1.5)
 dev.off()
+
+#-------------------- Problem 2 --------------------
+
+#Problem 2-1
+Matrix_2_1 <- XMatrix %*% solve(t(XMatrix) %*% XMatrix) %*% t(XMatrix)
+TraceOfMatrix_2_1 <- sum(diag(Matrix_2_1))
+print(TraceOfMatrix_2_1)
+
+#Problem 2-2
+I_n <- diag(504)
+Matrix_2_2 <- I_n - Matrix_2_1
+TraceOfMatrix_2_2 <- sum(diag(Matrix_2_2))
+print(TraceOfMatrix_2_2)
+
+#-------------------- Problem 3 --------------------
+
+eigenX <- eigen(t(XMatrix) %*% XMatrix)
+print(eigenX$values)
+png(paste("ScreePlot.png"))
+plot(1:11, eigenX$values, type = "l", col = rgb(0, 0, 1), xlab = "j", ylab = "eigenvalue", main = "Scree Plot")
+dev.off()
+
+
+
+
+
+
