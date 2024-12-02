a9105ddff7fe3991c4bb93dcd15f24a130c14810
Chinying Lin <104905519+cylin00@users.noreply.github.com>
2024-12-02 23:59:54 +0800

    hw10: list bull market

--- Econometrics_hw10_b10901069/hw10_b10901069.R
+++ Econometrics_hw10_b10901069/hw10_b10901069.R
@@ -14,3 +14,13 @@ x_ltr <- Matrix[, 10, drop = FALSE]
 x_ep <- Matrix[, 11, drop = FALSE]
 x_bmr <- Matrix[, 12, drop = FALSE]
 x_ntis <- Matrix[, 13, drop = FALSE]
+
+Y <- matrix(1, nrow = length(x_dfy), ncol = 1)
+# Bull Market from 1981 - 2022
+Y[20:80] <- 0 # August 1982 - August 1987
+Y[84:231] <- 0 # December 1987 - March 2000
+Y[249:253] <- 0 # September 2001 - January 2002
+Y[262:322] <- 0 # October 2002 - October 2007
+Y[339:470] <- 0 # March 2009 - February 2020
+Y[471:493] <- 0 # March 2020 - January 2022
+Y[494:500] <- 0 # February 2022 - August 2022
