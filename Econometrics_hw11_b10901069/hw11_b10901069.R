1c0d027437ea9f828af834026a5f01d916df4905
Chinying Lin <104905519+cylin00@users.noreply.github.com>
2024-12-09 19:59:53 +0800

    hw11: add BP statistic

--- Econometrics_hw11_b10901069/hw11_b10901069.R
+++ Econometrics_hw11_b10901069/hw11_b10901069.R
@@ -19,24 +19,36 @@ x_ntis <- Matrix[, 13, drop = FALSE]
 X <- cbind(x_dfy, x_infl, x_svar, x_tms, x_tbl, x_dfr, x_dp, x_ltr, x_ep, x_bmr, x_ntis)
 
 combined_data <- cbind(X, Y)
-colnames(combined_data) <- c(paste0("X_", 1:ncol(X)), "Y")
+colnames(combined_data) <- c(colnames(X), "Y")
 
-acf_data <- data.frame(Lag = integer(), ACF = numeric(), Series = character())
+ACFdata <- data.frame(Lag = integer(), ACF = numeric(), Series = character())
+BPresults <- data.frame(Series = character(), Q_12 = numeric(), Q_24 = numeric(), Lag_12_Statistic = numeric(), Lag_24_Statistic = numeric())
 
 for (i in 1:ncol(combined_data)) {
   ts_data <- combined_data[, i]
   series_name <- colnames(combined_data)[i]
   
-  acf_result <- acf(ts_data, lag.max = 24, plot = FALSE)
-  acf_values <- acf_result$acf[-1]
+  ACFresult <- acf(ts_data, lag.max = 24, plot = FALSE)
+  ACFvalues <- ACFresult$acf[-1]
   lags <- 1:24
   
-  acf_data <- rbind(acf_data, data.frame(Lag = lags, ACF = acf_values, Series = series_name))
+  ACFdata <- rbind(ACFdata, data.frame(Lag = lags, ACF = ACFvalues, Series = series_name))
+  
+  BP_test_12 <- Box.test(ts_data, lag = 12, type = "Box-Pierce")
+  BP_test_24 <- Box.test(ts_data, lag = 24, type = "Box-Pierce")
+  
+  BPresults <- rbind(BPresults, data.frame(
+    Series = series_name,
+    Lag_12_P_Value = BP_test_12$p.value,
+    Lag_24_P_Value = BP_test_24$p.value,
+    Q_12 = BP_test_12$statistic,
+    Q_24 = BP_test_24$statistic
+  ))
 }
 
 conf_level <- qnorm(0.975) / sqrt(nrow(combined_data))
 
-acf_plot <- ggplot(acf_data, aes(x = Lag, y = ACF, color = Series, group = Series)) +
+ACFplot <- ggplot(ACFdata, aes(x = Lag, y = ACF, color = Series, group = Series)) +
   geom_line(linewidth = 1) +
   geom_hline(yintercept = c(conf_level, -conf_level), linetype = "dashed", color = "black") +
   labs(title = "Autocorrelation Functions of All Time Series",
@@ -45,6 +57,8 @@ acf_plot <- ggplot(acf_data, aes(x = Lag, y = ACF, color = Series, group = Serie
   theme_minimal() +
   theme(legend.title = element_blank()) # Remove legend title
 
-ggsave("Combined_ACF_Plot.png", plot = acf_plot, width = 10, height = 6)
+ggsave("ACF_Plot.png", plot = ACFplot, width = 10, height = 6)
+print(ACFplot)
 
-print(acf_plot)
+cat("Box-Pierce Test Results:\n")
+print(BPresults)
